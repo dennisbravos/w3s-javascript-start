d46884468af97ff227fadbdf4d2448dcaeee371d
Dennis Bravos <67908162+dennisbravos@users.noreply.github.com>
2022-09-21 15:53:30 +0300

    Add files via upload

---
+++ gnirael.js
@@ -0,0 +1,10 @@
+window.alert(5 + 6);
+
+    alert(49 + 49); // niger balls biden oh no
+
+    let a, b, c;
+    a = 5;
+    b = 6;
+    c = a + b;
+    document.getElementById("demo1").innerHTML = c;
+        
